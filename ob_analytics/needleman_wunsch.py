62ce38ad7526b49f47985ac87a40abab6b6f64cf
mczielinski <mczielinski@gmail.com>
2023-08-28 18:44:08 -0400

    Update needleman_wunsch.py
    Changed around documentation and static typing a bit

--- ob_analytics/needleman_wunsch.py
+++ ob_analytics/needleman_wunsch.py
@@ -1,50 +1,55 @@
 import numpy as np
 import pandas as pd
+import numpy as np
+from typing import Callable, Sequence, Optional
 
-def sMatrix(a: list, b: list, filter_func: callable = None) -> np.ndarray:
+def sMatrix(a: np.ndarray, b: np.ndarray, filter_func: Optional[Callable[[pd.Timestamp, pd.Timestamp], int]] = None) -> np.ndarray:
     """
-    Construct a similarity matrix between two lists.
+    Construct a similarity matrix between two arrays.
     
     Parameters
     ----------
-    a : list
-        List a.
-    b : list
-        List b.
+    a : np.ndarray
+        First array.
+    b : np.ndarray
+        Second array.
     filter_func : callable, optional
-        A similarity function. Default is 1 if equal, -1 otherwise.
+        A similarity function that takes two timestamps and returns a scalar.
+        Default is 1 if timestamps are equal, -1 otherwise.
 
     Returns
     -------
     np.ndarray
-        A similarity matrix.
+        A similarity matrix with dimensions (len(a), len(b)).
     """
     if filter_func is None:
         filter_func = lambda f1, f2: 1 if f1 == f2 else -1
-    
+        
     matrix = np.array([[filter_func(b_val, a_val) for a_val in a] for b_val in b])
     return matrix
 
-def alignS(s_matrix: np.ndarray, gap: int = -1) -> np.ndarray:
+def alignS(s_matrix: np.ndarray[int], gap: int = -1) -> np.ndarray[int]:
     """
-    Align two sequences using the Needleman-Wunsch algorithm.
-    
+    Align 2 sequences using the Needleman-Wunsch matching algorithm.
+
     Parameters
     ----------
-    s_matrix : np.ndarray
-        Similarity matrix.
+    s_matrix : numpy.ndarray[int]
+        Similarity matrix with dimensions (len(a), len(b)).
     gap : int, optional
-        Penalty assigned to a gap (missing or extra value).
+        Penalty assigned to a gap (missing or extra value). Default is -1.
 
     Returns
     -------
-    np.ndarray
-        2 column matrix with aligned indices from each sequence.
+    numpy.ndarray[int]
+        2 column matrix. First column corresponds to the rows of the similarity matrix
+        (first sequence), Second column corresponds to the columns of the similarity matrix
+        (second sequence). Each row maps aligned indices from each sequence.
     """
-    s_len = s_matrix.shape[0]
-    q_len = s_matrix.shape[1]
-    f_matrix = np.zeros((s_len+1, q_len+1))
     
+    s_len, q_len = s_matrix.shape
+    f_matrix = np.zeros((s_len+1, q_len+1))
+        
     # Initialize first row and column of f_matrix
     f_matrix[:, 0] = np.arange(0, s_len+1) * gap
     f_matrix[0, :] = np.arange(0, q_len+1) * gap
@@ -68,6 +73,6 @@ def alignS(s_matrix: np.ndarray, gap: int = -1) -> np.ndarray:
             i -= 1
         else:
             j -= 1
+
     res.reverse()
     return np.array(res)
-
