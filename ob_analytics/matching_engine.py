0d91b34fa8c9bf121e4b8b0ef34d78466ec150c8
mczielinski <mczielinski@gmail.com>
2023-10-21 22:10:02 -0400

    Mostly working, tracking down more differences
    Few elements in matching engine and order types still either unmatched or classified differently, thinking on interpretation

--- ob_analytics/matching_engine.py
+++ ob_analytics/matching_engine.py
@@ -1,86 +1,62 @@
 import pandas as pd
 import numpy as np
+from typing import List, Union, Tuple
 from ob_analytics.needleman_wunsch import sMatrix
 from ob_analytics.needleman_wunsch import alignS
 
 def event_match(events: pd.DataFrame, cut_off_ms: int = 5000) -> pd.DataFrame:
     """
-    Match bid and ask events based on fill volumes and timestamps.
-    
-    Parameters
-    ----------
-    events : pd.DataFrame
-        Events dataframe.
-    cut_off_ms : int, optional
-        Maximum time difference in milliseconds for matching events. Defaults to 5000.
+    Match Market Orders (takers) to Limit Orders (makers).
 
-    Returns
-    -------
-    pd.DataFrame
-        Dataframe with an additional 'matching.event' column.
+    Parameters:
+    - events : pd.DataFrame
+        DataFrame of order book events.
+    - cut_off_ms : int
+        Events occurring outside of this time (in milliseconds) will be considered as candidate matches.
+
+    Returns:
+    - pd.DataFrame
+        DataFrame with matched events.
     """
     
-    def matcher() -> np.ndarray:
-        """
-        Match events using simple closest timestamp and alignment algorithm.
+    def fillId(src: pd.DataFrame, dst: pd.DataFrame) -> pd.DataFrame:
+        return src[src["fill"].isin(dst["fill"])]
 
-        Returns
-        -------
-        np.ndarray
-            Array of matched event ids.
-        """
+    def matcher(events: pd.DataFrame, cut_off_ms: int) -> List[Tuple[int, int]]:
         res = []
-        cols = ["event.id", "fill", "timestamp"]
-
-        bid_fills = events[(events["direction"]=="bid") & (events["fill"] != 0)][cols]
-        ask_fills = events[(events["direction"]=="ask") & (events["fill"] != 0)][cols]
-
-        # Identifiable bid and ask fills
-        def fill_id(src, dst):
-            return src[src['fill'].isin(dst['fill'])].sort_values(by=["fill", "timestamp"])
-
-        id_bid_fills = fill_id(bid_fills, ask_fills)
-        id_ask_fills = fill_id(ask_fills, bid_fills)
+        bid_fills = events[(events["direction"] == "bid") & (events["fill"] != 0)]
+        ask_fills = events[(events["direction"] == "ask") & (events["fill"] != 0)]
+        
+        id_bid_fills = fillId(bid_fills, ask_fills)
+        id_ask_fills = fillId(ask_fills, bid_fills)
 
         for volume in id_bid_fills["fill"].unique():
             bids = id_bid_fills[id_bid_fills["fill"] == volume]
             asks = id_ask_fills[id_ask_fills["fill"] == volume]
 
-            distance_matrix_ms = np.abs(np.subtract.outer(bids["timestamp"].astype(int).values, asks["timestamp"].astype(int).values))
+            distance_matrix_ms = np.abs(np.array([[((bid - ask).total_seconds() * 1000) for ask in asks["timestamp"]] for bid in bids["timestamp"]]))
+            # Simple matching: For each bid, find the index of the closest ask
+            min_indices = np.argmin(np.where(distance_matrix_ms <= cut_off_ms, distance_matrix_ms, np.inf), axis=1)
+            ask_event_ids = [asks.iloc[idx]["event.id"] for idx in min_indices]
 
-            # Simple matching
-            ask_event_ids = np.array([asks["event.id"].iloc[np.argmin(row)] if np.min(row) <= cut_off_ms else np.nan for row in distance_matrix_ms])
-
-            if len(np.unique(ask_event_ids[~np.isnan(ask_event_ids)])) == len(ask_event_ids[~np.isnan(ask_event_ids)]):
-                matches = np.column_stack((bids["event.id"].values, ask_event_ids))
-                matches = matches[~np.isnan(matches[:, 1])]
-                res.append(matches)
+            # Check for duplicates in the matched asks
+            if not any(pd.Series(ask_event_ids).duplicated()):
+                res.extend(zip(bids["event.id"].tolist(), ask_event_ids))
             else:
-                # Alignment algorithm
-                sm = sMatrix(bids["timestamp"].astype(int).values, asks["timestamp"].astype(int).values, filter_func=lambda f1, f2: cut_off_ms if abs(f1 - f2) == 0 else cut_off_ms / abs(f1 - f2))
-                aligned_idx = alignS(sm, gap=-1)
-                aligned_idx -= aligned_idx
-                
-                matched_bids = bids.iloc[aligned_idx[:, 1]]
-                matched_asks = asks.iloc[aligned_idx[:, 0]]
-                in_bounds = np.abs(matched_bids["timestamp"].astype(int).values - matched_asks["timestamp"].astype(int).values) <= cut_off_ms
+                sm = sMatrix(bids["timestamp"].to_list(), asks["timestamp"].to_list(),
+                            lambda f1, f2: cut_off_ms / np.abs((f1 - f2).total_seconds() * 1000))
+                aligned_idx = alignS(sm)
+                matched_bids = bids.iloc[aligned_idx["a"].values - 1]
+                matched_asks = asks.iloc[aligned_idx["b"].values - 1]
+                time_diffs = matched_bids["timestamp"].values - matched_asks["timestamp"].values
+                in_bounds = np.abs(pd.Series(time_diffs).dt.total_seconds() * 1000) <= cut_off_ms
+                res.extend(zip(matched_bids.iloc[in_bounds.values,0].to_list(), matched_asks.iloc[in_bounds.values,0].to_list()))
+        return res
 
-                res.append(np.column_stack((matched_bids[in_bounds]["event.id"].values, matched_asks[in_bounds]["event.id"].values)))
-
-        return np.vstack(res)
-
-    matched = matcher()
-    matched = np.unique(matched,axis=0)
-
-    # Ensure no duplicates
-    assert len(matched[:, 0]) == len(np.unique(matched[:, 0]))
-    assert len(matched[:, 1]) == len(np.unique(matched[:, 1]))
-
+    matched = matcher(events, cut_off_ms)
     events["matching.event"] = np.nan
-    bid_matches = np.searchsorted(events["event.id"].values, matched[:, 0])
-    ask_matches = np.searchsorted(events["event.id"].values, matched[:, 1])
-
-    events.iloc[bid_matches, events.columns.get_loc("matching.event")] = matched[:, 1]
-    events.iloc[ask_matches, events.columns.get_loc("matching.event")] = matched[:, 0]
-
+    for bid, ask in matched:
+        events.loc[events["event.id"] == bid, "matching.event"] = ask
+        if not np.isnan(ask):
+            events.loc[events["event.id"] == ask, "matching.event"] = bid
     return events
